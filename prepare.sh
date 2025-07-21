e57a3462f8abeb11b736fee07a9d6e2df855ef89
nyelidl <120070650+nyelidl@users.noreply.github.com>
2025-07-21 11:09:23 +0900

    Delete prepare.sh

--- prepare.sh
+++
@@ -1,2 +0,0 @@
-for i in A B C; do antechamber -i Lig${i}.mol2 -o Ligand${i}.mol2 -fi mol2 -fo mol2 -c bcc -pf yes -nc -2 -at gaff2 -j 5 -rn CH${i}; done
-for i in A B C; do parmchk2 -i Ligand${i}.mol2 -f mol2 -o Ligand${i}.frcmod -s 2; done
